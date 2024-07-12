04ccece20f745d906910dc892128efa2b70ccc47
Saisindhu Kanuri <saisindhu.kanuri@gmail.com>
2024-07-12 23:08:21 +0530

    firstcommit

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,17 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
 
-//INTERNAL IMPORT
-import { NavBar,Footer } from "../Components";
-import {CrowdFundingProvider} from  '../Context/CrowdFunding'
+// INTERNAL IMPORT
+import { NavBar, Footer } from "../Components";
+import { CrowdFundingProvider } from '../Context/CrowdFunding';
 
 export default function App({ Component, pageProps }) {
   return (
-  <>
-  <CrowdFundingProvider>
-  <NavBar />
-  <Component {...pageProps} />
-  <Footer />
-  </CrowdFundingProvider>
-  </>
+    <>
+      <CrowdFundingProvider>
+        <NavBar />
+        <Component {...pageProps} />
+        <Footer />
+      </CrowdFundingProvider>
+    </>
   );
 }
